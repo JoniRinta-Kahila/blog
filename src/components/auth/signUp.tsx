56bf8975dcf75c06d89f1ae63e10f05f3cb1f186
JoniRinta-Kahila <joni.rintakahila@opp.eduvantaa.fi>
2021-10-09 13:02:04 +0300

    update signup

--- src/components/auth/signUp.tsx
+++ src/components/auth/signUp.tsx
@@ -18,7 +18,8 @@ const SignUp: React.FC<SignUpProps> = () => {
   const [password, setPassword] = useState<string>('');
   const [passwordRepeat, setPasswordRepeat] = useState<string>('');
   const [regReady, setRegReady] = useState<boolean>(false);
-  const [regOnProgress, setRegOnProgress] = useState<boolean>(false);
+  // TODO add progress spinner
+  // const [regOnProgress, setRegOnProgress] = useState<boolean>(false);
 
   const { setNeedToSignUp } = useAuthPopupStateContext();
 
