6cf8ca8b67f497b17e70065f71e45cf166e54ce3
JoniRinta-Kahila <joni.rintakahila@opp.eduvantaa.fi>
2021-10-10 15:15:39 +0300

    create header app properties

--- src/appProperties.ts
+++ src/appProperties.ts
@@ -1,3 +1,6 @@
+export const pageHeader = "My Blog"
+export const pageSubheader = 'console.log(\'Hello, User!\');'
+
 export const endpoints = {
   registration: 'https://us-central1-blog-43f84.cloudfunctions.net/userRegistration',
 };
--- src/components/basic/header.tsx
+++ src/components/basic/header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { pageHeader, pageSubheader } from '../../appProperties';
 import styles from './header.module.scss';
 
 const Header: React.FC = () => {
   return (
   <div className={styles.container}>
-    <h1>My Blog</h1>
-    <h2>console.log('Hello, User!');</h2>
+    <h1>{pageHeader}</h1>
+    <h2>{pageSubheader}</h2>
   </div>
   )
 }
