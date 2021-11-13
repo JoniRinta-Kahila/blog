3b14c75f83e98e63609936ea4a3c6c902b239e54
JoniRinta-Kahila <joni.rintakahila@opp.eduvantaa.fi>
2021-11-13 02:11:35 +0200

    update index

--- src/index.tsx
+++ src/index.tsx
@@ -11,35 +11,12 @@ import 'react-tabs/style/react-tabs.scss';
 // import 'ag-grid-community/dist/styles/ag-grid.css';
 import './components/agGridReact/styles/ag-grid.min.css'
 import './components/agGridReact/styles/ag-theme-material.css';
-import GetEmailHash from './components/gravatar/getEmailHash';
-
-const isApi = (): boolean => {
-  let host = window.location.host;
-  // let protocol = window.location.protocol;
-  let parts = host.split('.');
-  let subdomain = '';
-  // If we get more than 3 parts, then we have a subdomain
-  // INFO: This could be 4, if you have a co.uk TLD or something like that.
-
-  if (parts.length >= 3) {
-    subdomain = parts[0];
-    if (subdomain === 'api') {
-      console.log('we have api');
-      return true;
-    }
-  }
-  return false;
-}
 
 ReactDOM.render(
   <React.StrictMode>
-    {
-      isApi()
-      ? <img src={`https://www.gravatar.com/avatar/${GetEmailHash('joni.rinta-kahila@hotmail.com')}?s=200&r=pg&d=404`} alt='gravatar' />
-      : <Router>
-          <App />
-        </Router>
-    }
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
