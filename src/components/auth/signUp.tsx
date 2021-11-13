41b8d5cffc6e760d779f3bcb680c9747f710b4af
JoniRinta-Kahila <joni.rintakahila@opp.eduvantaa.fi>
2021-11-14 00:24:03 +0200

    update signup form

--- src/components/auth/signUp.tsx
+++ src/components/auth/signUp.tsx
@@ -29,9 +29,11 @@ const SignUp: React.FC = () => {
   
   const ai = FirebaseServices.getAuthInstance();
 
-  const handleSignUp = async () => {
+  const handleSignUp = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
     if (!email || !username || !password || password !== passwordRepeat) {
-      alert('cant send');
+      alert('fill all fields!');
       return;
     }
 
@@ -79,64 +81,65 @@ const SignUp: React.FC = () => {
             ? progressLayer
             : null
         }
-        <div className={styles.flexRow}>
-          <label className={styles.label} htmlFor='email'>
-            <MdAlternateEmail style={{width:'12px', height:'13px'}} color='grey' />
-          </label>
+        <form onSubmit={(event) => handleSignUp(event)}>
+          <div className={styles.flexRow}>
+            <label className={styles.label} htmlFor='email'>
+              <MdAlternateEmail style={{width:'12px', height:'13px'}} color='grey' />
+            </label>
+            <input
+              id='email'
+              className={styles.input}
+              placeholder='Email'
+              type='text'
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+          <div className={styles.flexRow}>
+            <label className={styles.label} htmlFor='username'>
+              <AiOutlineUser style={{width:'12px', height:'13px'}} color='grey' />
+            </label>
+            <input
+              id='username'
+              className={styles.input}
+              placeholder='Username'
+              type='text'
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </div>
+          <div className={styles.flexRow}>
+            <label className={styles.label} htmlFor='password'>
+              <BiKey style={{width:'15px', height:'15px'}} color='grey' />
+            </label>
+            <input
+              id='password'
+              className={styles.input}
+              placeholder='Password'
+              type='password'
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <div className={styles.flexRow}>
+            <label className={styles.label} htmlFor='passwordRep'>
+              <AiOutlineSecurityScan style={{width:'15px', height:'15px'}} color='grey' />
+            </label>
+            <input
+              id='passwordRep'
+              className={styles.input}
+              placeholder='Repeat password'
+              type='password'
+              value={passwordRepeat}
+              onChange={(event) => setPasswordRepeat(event.target.value)}
+            />
+          </div>
           <input
-            id='email'
-            className={styles.input}
-            placeholder='Email'
-            type='text'
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
+            className={styles.submit}
+            type='submit'
+            value='SIGNUP'
           />
-        </div>
-        <div className={styles.flexRow}>
-          <label className={styles.label} htmlFor='username'>
-            <AiOutlineUser style={{width:'12px', height:'13px'}} color='grey' />
-          </label>
-          <input
-            id='username'
-            className={styles.input}
-            placeholder='Username'
-            type='text'
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </div>
-        <div className={styles.flexRow}>
-          <label className={styles.label} htmlFor='password'>
-            <BiKey style={{width:'15px', height:'15px'}} color='grey' />
-          </label>
-          <input
-            id='password'
-            className={styles.input}
-            placeholder='Password'
-            type='password'
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <div className={styles.flexRow}>
-          <label className={styles.label} htmlFor='passwordRep'>
-            <AiOutlineSecurityScan style={{width:'15px', height:'15px'}} color='grey' />
-          </label>
-          <input
-            id='passwordRep'
-            className={styles.input}
-            placeholder='Repeat password'
-            type='password'
-            value={passwordRepeat}
-            onChange={(event) => setPasswordRepeat(event.target.value)}
-          />
-        </div>
-        <input
-          className={styles.submit}
-          type='submit'
-          value='SIGNUP'
-          onClick={() => handleSignUp()}
-        />
+        </form>
         <div className={styles.actions}>
           <a href='*' onClick={(event) => {
             event.preventDefault();
